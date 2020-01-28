48644715d02c628f26275a843004f3738ec82061
InfPALS <infpals.ed@gmail.com>
2020-01-28 17:56:55 +0000

    Adding backend.js content.

--- backend.js
+++ backend.js
@@ -1,5 +1,13 @@
 function purchase() {
+    window.location.href = "./order.html";
 }
 
 function order() {
+    address = document.getElementById("address").value;
+    postcode = document.getElementById("postcode").value;
+    city = document.getElementById("city").value;
+
+    full_address = address + ", " + postcode + " in " + city;
+
+    alert("We will ship to " + full_address + "!");
 }
